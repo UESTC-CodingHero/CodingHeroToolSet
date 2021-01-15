45876d769176e0caed2670e09f8af9fd37239d1b
XueliCheng <chengxl842363@gmail.com>
2021-01-15 22:23:26 +0800

    Update Readme

--- SimpleHPC/setup.py
+++ SimpleHPC/setup.py
@@ -11,7 +11,7 @@ setup(name="hpc",
       install_requires=["openpyxl"],
       include_package_data=True,
       package_data={"": ["*.xlsm"]},
-      exclude_package_data={'': ['README.txt']},
+      exclude_package_data={'': ['README.md']},
       entry_points={'console_scripts': [
           'progress_server = hpc.app.app_master:main',
       ]},
